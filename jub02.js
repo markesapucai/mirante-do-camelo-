e37340bc45539fc4a58de207a1d6065c195f2126
Mark Leite <marktobusiness113@gmail.com>
2023-08-05 16:34:15 -0300

    finishing css and start js

--- jub02.js
+++ jub02.js
@@ -1,13 +1,8 @@
-function toggleMode() {
-    const html = document.documentElement
-  
-   /*if(html.classList.contains('ligth')) {
-      html.classList.remove('ligth')
-    } else {
-      html.classList.add('ligth')
-    }*/
-  
-    html.classList.toggle('ligth')
-  }
+let botoes = window.document.getElementById('contato')
+botoes.addEventListener('mouseenter', entrou);
 
-  
+
+function entrou() {
+  botoes.style.backgroundColor = 'red'
+}
+
